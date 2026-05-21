978b6190b44df10b65bf5c61d66e2e704f4e5bc1
Agustin <agusbarralia@gmail.com>
2026-05-21 19:42:48 -0300

    feat: módulo vpc

---
+++ BreadBoss/modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "prefix" {
+  type = string
+}
+
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "az_count" {
+  type    = number
+  default = 2
+}
+
+variable "aws_region" {
+  type = string
+}
