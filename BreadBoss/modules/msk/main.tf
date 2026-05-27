4a7c3e36b6be2c57f5145f597168b5378dfd0f61
Agustin <agusbarralia@gmail.com>
2026-05-27 12:31:00 -0300

    fix MSK

--- BreadBoss/modules/msk/main.tf
+++ BreadBoss/modules/msk/main.tf
@@ -13,6 +13,17 @@ resource "aws_security_group" "msk" {
   tags = { Name = "${var.prefix}-msk-sg" }
 }
 
+# Lambda ESM poller ENIs use the MSK SG (not Lambda SG), so the MSK SG
+# must allow inbound from itself on 9098 for the poller to reach the cluster.
+resource "aws_vpc_security_group_ingress_rule" "msk_self" {
+  security_group_id            = aws_security_group.msk.id
+  referenced_security_group_id = aws_security_group.msk.id
+  ip_protocol                  = "tcp"
+  from_port                    = 9098
+  to_port                      = 9098
+  description                  = "MSK ESM poller self-access"
+}
+
 resource "aws_msk_serverless_cluster" "this" {
   cluster_name = "${var.prefix}-kafka"
 
